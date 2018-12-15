42923fbd1109bcd5bac89e84af0e26fcf06d4c9a
xreignmanx <rtballentine@yahoo.com>
2018-12-15 18:14:18 -0500

    order Confirm

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -38,39 +38,39 @@ function listProducts() {
     function dspTable() {
     connection.query("SELECT * FROM products", function(err, res) {
       if (err) throw err;
-            // for (var i = 0; i < res.length; i++) {
+            for (var i = 0; i < res.length; i++) {
 
-            //     var itemId = res[i].item_id,
-            //         productName = res[i].product_name,
-            //         departmentName = res[i].department_name,
-            //         price = res[i].price,
-            //         stockQuantity = res[i].stock_quantity;
+                var itemId = res[i].item_id,
+                    productName = res[i].product_name,
+                    departmentName = res[i].department_name,
+                    price = res[i].price,
+                    stockQuantity = res[i].stock_quantity;
 
-            //   table.push(
-            //       [itemId, productName, departmentName, price, stockQuantity]
-            // );
+              table.push(
+                  [itemId, productName, departmentName, price, stockQuantity]
+            );
     
       // Log all results of the SELECT statement
-      for (var index = 0; index < res.length; index++) {
-        var id = res[i].item_id,
-            product = res[i].product_name,
-            department = res[i].department_name,
-            price = res[i].price,
-            quantity = res[i].stock_quantity;
-      table.push([id, product, department, price, quantity]
-        );
+      // for (var index = 0; index < res.length; index++) {
+      //   var id = res[i].item_id,
+      //       product = res[i].product_name,
+      //       department = res[i].department_name,
+      //       price = res[i].price,
+      //       quantity = res[i].stock_quantity;
+      // table.push([id, product, department, price, quantity]
+      //   );
+        }
+      })
     }
-  })
-}
     
       console.log(table);
-      runSearch()
+      runSearch();
 }
 
 // ------------Prompt questions for purchase----------------
   
   // Create questions with inquirer
-  function runSearch() {
+function runSearch() {
     inquirer
     .prompt([{
       name: "itemID",
@@ -81,28 +81,34 @@ function listProducts() {
       name: "quantity",
       type: "answer",
       message: "How many units would you like to buy?",
-    }])
-      .then(function(purchaseProduct) {
+  }]).then(function(purchaseProduct) {
         connection.query("SELECT * FROM products WHERE item_id=?", purchaseProduct.itemID, function(err, res) {
           for (var i = 0; i < res.length; i++) {
 
-              if (purchaseProduct.itemID > res[i].stock_quantity) {
+              if (purchaseProduct.quantity > res[i].stock_quantity) {
                    console.log("Sorry! Not enough in stock. Please try again later.");
                   listProducts();
               } else {
-                var newStock = (res[i].stock_quantity - purchaseProduct.quantity);
-                var itemPurchased = (userPurchase.inputId);
-                //console.log(newStock);
-                confirmPrompt(newStock, itemPurchased);
+                var newCount = (res[i].stock_quantity - purchaseProduct.quantity);
+                var itemPurchased = (purchaseProduct.inputId);
+                orderConfirm(newCount, itemPurchased);
                   }
                 }
               }
-                
-        
- 
-          );
+           );
         })
-      }
+    function orderConfirm(newCount, itemPurchased) {
+    connection.query("UPDATE products SET ? WHERE ?", [{
+      stock_quantity: newCount
+  }, {
+      item_id: itemPurchased
+  }], function(err, res) {});
+  console.log(orderConfirm);
+
+  console.log("Your order has been placed. Thank you.");
+  // listProducts();
+};
+}
       
 
 
